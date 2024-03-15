162b2e9a1d87d839a8b3d160e9379723d7349479
YamaMaru15 <liberte1la1vie7@gmail.com>
2024-03-15 12:03:07 +0900

    プロップスの学習➀

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ColorfulMessage } from './components/ColorfulMessage';
 
 
 // 変数はパスカルケース
+const style = {
+  color: "green",
+  fontSize: "56px",
+};
 
 export const App = () => {
   const onClickButton = () => alert();
   return (
     <>
-      <h1>やっほそ！</h1>
+      <h1 style = {style}>やっほそ！</h1>
+      <ColorfulMessage />
       <p>およぉ</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
-
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,11 @@
+export const ColorfulMessage = () => {
+    const contentStyleB = {
+        color: "green",
+        fontSize: "18px"
+    };
+    return (
+        <>
+            <p style={contentStyleB}>やあ！</p>
+        </>
+    );
+};
