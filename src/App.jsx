da7dd1bf56a4a5413584ed4fc721f2930f6b3c96
YamaMaru15 <liberte1la1vie7@gmail.com>
2024-03-17 12:31:22 +0900

    Stateの学習(useState()、再レンダリング、useEffect())

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
+import { useEffect, useState } from "react";
 import './App.css'
 import { ColorfulMessage } from './components/ColorfulMessage';
 
-
-// 変数はパスカルケース
-const style = {
-  color: "green",
-  fontSize: "10px",
-};
-
 export const App = () => {
-  const onClickButton = () => alert();
+  // 配列を受け取る関数、分割代入で受け取る 
+  const [num, setNum] = useState(0);
+  const [isShowCharacter, setIsShowCharacter] = useState(true);
+  const onClickCountUp = () => {
+    setNum((prev) => prev + 1);
+  };
+
+  // やぁの文字列をの表示非表示を切り替える
+  const onClickToggle = () => {
+    setIsShowCharacter(!isShowCharacter);
+  };
+
+  // 3の倍数の時にだけ、やぁを表示させる。
+  useEffect(() => {
+    if (num % 3 === 0) {
+      isShowCharacter || setIsShowCharacter(true);
+    } else {
+      isShowCharacter && setIsShowCharacter(false);
+    }
+  }, [num]);
+
   return (
     <>
-      <h1 style = {style}>やっほそ！</h1>
       {/* color,messageというプロップスを渡す */}
       <ColorfulMessage color="pink">やあ、君だね！</ColorfulMessage>
       <p>およぉ</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+       {/* state変数の表示。jsなので{}で囲う */}
+      <p>{num}</p>
+      <button onClick={onClickToggle}>on・off</button>
+      {/* tureの時だけ文字が表示 */}
+      {isShowCharacter && <p>やぁ</p>} 
     </>
   );
 };
